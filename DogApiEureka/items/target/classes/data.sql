ba238ea9986c2894a4cdf626a9f163675fbded54
ReeceMccormack <122635919+ReeceMccormack@users.noreply.github.com>
2023-11-23 12:44:28 +0000

    Add files via upload

---
+++ DogApiEureka/items/target/classes/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO Dog (id, name, breed, origin) VALUES (1, 'Patch','Poodle', 'Manchester');
+INSERT INTO Dog (id, name, breed, origin) VALUES (2, 'FLynn','Flatcoat', 'Fife');
+INSERT INTO Dog (id, name, breed, origin) VALUES (3, 'Plum','Yorkshire Terrier', 'Southhampton');
+INSERT INTO Dog (id, name, breed, origin) VALUES (4, 'Sadie','Swedish Mountain Dog', 'London');
+
